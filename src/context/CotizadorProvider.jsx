1dfe246f9366f2b08af3d031cad28278fbe08fcf
javierborjas <cristman11@gmail.com>
2024-02-09 02:25:05 -0300

    Agregando un Spinner de Carga

--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -16,6 +16,8 @@ const CotizadorProvider = ({children}) => {
 
     const [error, setError] = useState('')
     const [resultado, setResultado] = useState(0)
+    const [cargando, setCargando] = useState(false)
+    
 
     const handleChangeDatos = e => {
         setDatos({
@@ -46,7 +48,12 @@ const CotizadorProvider = ({children}) => {
         // Formatear Dinero
         resultado = formatearDinero(resultado)
 
-        setResultado(resultado)
+        setCargando(true)
+
+        setTimeout(() => {
+            setResultado(resultado)
+            setCargando(false) 
+        }, 3000);     
     }
 
     return(
@@ -57,7 +64,8 @@ const CotizadorProvider = ({children}) => {
                 error,
                 setError,
                 cotizarSeguro,
-                resultado
+                resultado,
+                cargando
             }}
         >
             {children}
